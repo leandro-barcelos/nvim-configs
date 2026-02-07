bd22901b8303b15e3f2ba2110e74a8e2dcaabe3b
Leandro Barcelos Ferreira <leandrobarcelos.f@hotmail.com>
2026-02-06 21:29:36 -0300

    formatted the formatting plugin file

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,33 +1,33 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				c = { "clang-format" },
-				cpp = { "cmake-format" },
-				cmake = { "cmakelang" },
-				javascript = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				python = { "ruff" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			},
-		})
+    conform.setup({
+      formatters_by_ft = {
+        c = { "clang-format" },
+        cpp = { "cmake-format" },
+        cmake = { "gersemi" },
+        javascript = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        python = { "ruff" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    })
 
-		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, {})
-	end,
+    vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, {})
+  end,
 }
