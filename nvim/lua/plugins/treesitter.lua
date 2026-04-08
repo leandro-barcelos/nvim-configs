d721ed2e2f3e3eb2c62c63cd02c91f3685518e95
Leandro Barcelos Ferreira <leandrobarcelos.f@hotmail.com>
2026-04-08 11:57:50 -0300

    added treesitter-textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   config = function()
     vim.filetype.add({
       extension = {
@@ -14,6 +17,27 @@ return {
     local languages = { "c", "cpp", "cmake", "lua", "python", "json", "jsonc", "slang" }
 
     ts.install(languages)
+
+    require("nvim-treesitter-textobjects").setup({
+      select = {
+        lookahead = true,
+      },
+    })
+
+    local select = require("nvim-treesitter-textobjects.select")
+    local maps = {
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["ac"] = "@class.outer",
+      ["ic"] = "@class.inner",
+      ["aa"] = "@parameter.outer",
+      ["ia"] = "@parameter.inner",
+    }
+    for lhs, query in pairs(maps) do
+      vim.keymap.set({ "o", "x" }, lhs, function()
+        select.select_textobject(query)
+      end, { desc = "TS textobject " .. query })
+    end
 
     local group = vim.api.nvim_create_augroup("dotfiles_treesitter", { clear = true })
     vim.api.nvim_create_autocmd("FileType", {
